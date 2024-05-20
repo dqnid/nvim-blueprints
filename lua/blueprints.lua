01f14195178ae55e305bc585ed4abc3ca6b26eff
dqnid <dani.heras@hotmail.com>
2024-05-20 11:51:34 +0200

    feat(plugin config) plugin configured with global vim variables

--- lua/blueprints.lua
+++ lua/blueprints.lua
@@ -1,23 +1,20 @@
 -- This code will only be executed when is required somewhere
 
 local selectors = require("./selectors")
-local augroup = vim.api.nvim_create_augroup("Blueprints", { clear = true })
 
-local srcDir = "~/Documents/Proyectos/Training/nvim-blueprints/blueprints"
+local srcDir = "/home/danih/Documents/Proyectos/nvim-blueprints/blueprints"
+vim.g.blueprintsDir = srcDir
 
-local function setup()
-	vim.api.nvim_create_autocmd(
-		"VimEnter",
-		{ group = augroup, desc = "Creates a file structure with base code", once = true } -- FIXME: not working
-	)
+local function setup(opts)
+	vim.g.blueprintsDir = opts.blueprintsDir
 end
 
 local function createFromTemplate()
-	selectors.blueprints("/home/danih/Documents/Proyectos/nvim-blueprints/blueprints")
+	selectors.blueprints(vim.g.blueprintsDir)
 end
 
 local function createFromTemplateTelescope()
-	selectors.blueprints("/home/danih/Documents/Proyectos/nvim-blueprints/blueprints")
+	selectors.blueprints(vim.g.blueprintsDir)
 end
 
 return {
